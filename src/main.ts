878fc07585153c4abadc3c1228e8176d1fd26838
jarangit <52028847+jarangit@users.noreply.github.com>
2025-05-09 03:27:56 +0700

    feat: update CORS configuration to use CLIENT_URL environment variable

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173', // ✅ ใส่ origin ของ frontend
+    origin: process.env.CLIENT_URL, // ✅ ใส่ origin ของ frontend
     credentials: true, // ถ้ามี cookie/session
   });
   await app.listen(process.env.PORT ?? 3000);
