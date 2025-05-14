8dc93073e049962d8de254a1cb09b9a183dd9c24
jarangit <jaran.dch@gmail.com>
2025-05-14 15:37:30 +0700

    chore: add comment for development branch in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  // dev branch
   app.enableCors({
     origin: process.env.CLIENT_URL, // ✅ ใส่ origin ของ frontend
     credentials: true, // ถ้ามี cookie/session
