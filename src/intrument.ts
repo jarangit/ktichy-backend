91d278688f2fae71873625ceb7ffb16a84e06820
jarangit <jaran.dch@gmail.com>
2025-06-06 14:24:31 +0700

    feat: update Sentry DSN configuration to use environment variable

--- src/intrument.ts
+++ src/intrument.ts
@@ -2,9 +2,8 @@
 import * as Sentry from '@sentry/nestjs';
 
 Sentry.init({
-  dsn: 'https://906ec56fa001c04ee86b7381843db28f@o4509441755709440.ingest.us.sentry.io/4509441760886784',
-
-  // Setting this option to true will send default PII data to Sentry.
-  // For example, automatic IP address collection on events
+  dsn: process.env.SENTRY_DSN,
+  tracesSampleRate: 0.1, // 10% performance sampling
+  sampleRate: 1.0, // Error sampling 100%
   sendDefaultPii: true,
 });
